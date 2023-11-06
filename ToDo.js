9481b9d593ca024d5d970032f83c56ae52db98f6
sreyasri19 <149997129+sreyasri19@users.noreply.github.com>
2023-11-06 22:45:08 +0530

    ToDo.js

--- ToDo.js
+++ ToDo.js
@@ -1,32 +1,18 @@
-import React, { useState } from 'react';
-import { BiEdit } from 'react-icons/bi';
-import { AiFillDelete } from 'react-icons/ai';
+import React from 'react'
 
-const ToDo = ({ text, updateMode, deleteToDo }) => {
-  const [completed, setCompleted] = useState(false);
+import {BiEdit} from "react-icons/bi"
+import {AiFillDelete} from "react-icons/ai"
 
-  const handleComplete = () => {
-    setCompleted(true);
-  };
+const ToDo = ({text, updateMode, deleteToDo}) => {
+    return (
+        <div className="todo">
+            <div className="text">{text}</div>
+            <div className="icons">
+                <BiEdit className='icon' onClick={updateMode} />
+                <AiFillDelete className='icon' onClick={deleteToDo} />
+            </div>
+        </div>
+    )
+}
 
-  return (
-    <div className={`todo${completed ? ' completed' : ''}`}>
-      <div className="text">{text}</div>
-      <div className="icons">
-        {!completed && (
-          <button
-            className={`completed-button${completed ? ' active' : ''}`}
-            onClick={handleComplete}
-          >
-            Mark as Complete
-          </button>
-        )}
-        {completed && <span className="completed-text">Completed</span>}
-        <BiEdit className="icon" onClick={updateMode} />
-        <AiFillDelete className="icon" onClick={deleteToDo} />
-      </div>
-    </div>
-  );
-};
-
-export default ToDo;
+export default ToDo
